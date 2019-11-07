31c5bc30d19fedf30aa4308ac1a92ecd7d9f7d04
Debonet <719893090@qq.com>
2019-11-07 15:01:34 +0800

    start with cross compiling.

---
+++ func.asm
@@ -0,0 +1,13 @@
+SECTION .data
+SECTION .text
+global _maxofthree
+_maxofthree:
+    mov eax,[esp+4]
+    mov ecx,[esp+8]
+    mov edx,[esp+12]
+label_2:
+    cmp eax,ecx
+    cmovl   eax,ecx
+    cmp eax,edx
+    cmovl   eax,edx
+    ret
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int maxofthree(int,int,int);
+int main(){
+    printf("%d\n",maxofthree(1,-4,-7));
+    printf("%d\n",maxofthree(1,2,3));
+    return 0;
+}
